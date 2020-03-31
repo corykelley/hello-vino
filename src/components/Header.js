90ef048307a54ef827d6a2b377032ad543a8da2b
johncoryk <coryk0789@gmail.com>
2020-03-30 20:53:26 -0400

    Updated logic for wine recommendation. Some styling changes.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import HelloVinoLogo from '../photos/hello-vino-logo.png';
+
 const Header = () => {
   return (
     <div className="flex justify-between items-center py-8 shadow-md">
       <h2 className="ml-24 font-serif">
-        <Link to="/">Hello Vino</Link>
+        <Link to="/">
+          <img className="logo" src={HelloVinoLogo} alt="Hello Vino Logo" />
+        </Link>
       </h2>
       <ul className="mr-24 flex flex-row uppercase">
         <li className="px-3 links">
--- src/pages/WhatToDrink.js
+++ src/pages/WhatToDrink.js
@@ -1,8 +1,121 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Footer from '../components/Footer';
 
 const WhatToDrink = () => {
+  const [option1, setOption1] = useState('---');
+  const [option2, setOption2] = useState('---');
+  const [option3, setOption3] = useState('---');
+  // const [options, setOptions] = useState();
+
+  const onSubmitForm = e => {
+    if (option1 === '---' || option2 === '---' || option3 === '---') {
+      alert('Please select all options!');
+      e.preventDefault();
+    } else {
+      switch (option1) {
+        case 'Nothing':
+          switch (option2) {
+            case 'Yes':
+              switch (option3) {
+                case 'Spring':
+                  alert('Spring');
+                  break;
+                case 'Summer':
+                  alert('Summer');
+                  break;
+                case 'Fall':
+                  alert('Fall');
+                  break;
+                case 'Winter':
+                  alert('Winter');
+                  break;
+              }
+              break;
+            case 'No':
+              switch (option3) {
+                case 'Spring':
+                  alert('Whiteasdfasdf Wine');
+                  break;
+              }
+          }
+          break;
+        case 'Chicken or Other Fowl':
+          switch (option2) {
+            case 'Yes':
+              switch (option3) {
+                case 'Spring':
+                  alert('Coke');
+                  break;
+              }
+          }
+          break;
+        case 'Red Meat':
+          switch (option2) {
+            case 'Yes':
+              switch (option3) {
+                case 'Spring':
+                  alert('REEEED');
+                  break;
+              }
+          }
+          break;
+        case 'Fish':
+          switch (option2) {
+            case 'Yes':
+              switch (option3) {
+                case 'Spring':
+                  alert('REEEED');
+                  break;
+              }
+          }
+          break;
+        case 'Cheese':
+          switch (option2) {
+            case 'Yes':
+              switch (option3) {
+                case 'Spring':
+                  alert('REEEED');
+                  break;
+              }
+          }
+          break;
+        case 'Something Spicy':
+          switch (option2) {
+            case 'Yes':
+              switch (option3) {
+                case 'Spring':
+                  alert('REEEED');
+                  break;
+              }
+          }
+          break;
+        case 'Pasta':
+          switch (option2) {
+            case 'Yes':
+              switch (option3) {
+                case 'Spring':
+                  alert('REEEED');
+                  break;
+              }
+          }
+      }
+
+      // if (option1 === 'Nothing') {
+      //   if (option2 === 'Yes') {
+      //     if (option3 === 'Spring') {
+      //       alert('Red Wine');
+      //     } else if (option3 === 'Summer') {
+      //       alert('White Wine');
+      //     }
+      //   }
+      // }
+    }
+    console.log({ option1 });
+    console.log({ option2 });
+    console.log({ option3 });
+  };
+
   return (
     <div className="w-8/12 mt-12 mb-4 mx-auto">
       <form className="shadow-xl rounded px-8 pt-6 pb-8 my-8 mx-auto">
@@ -14,9 +127,13 @@ const WhatToDrink = () => {
             <p className="text-xl">What Are You Eating?</p>
           </label>
           <div className="inline-block relative w-full">
-            <select className="pointer block appearance-none w-full border border-gray-800 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+            <select
+              onChange={e => setOption1(e.target.value)}
+              className="pointer block appearance-none w-full border border-gray-800 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+            >
+              <option>---</option>
               <option>Nothing</option>
-              <option>Chicken or other fowl</option>
+              <option>Chicken or Other Fowl</option>
               <option>Red Meat</option>
               <option>Fish</option>
               <option>Cheese</option>
@@ -44,8 +161,12 @@ const WhatToDrink = () => {
             <p className="text-xl">Is it a Special Occasion?</p>
           </label>
           <div className="inline-block relative w-full">
-            <select className="pointer block appearance-none w-full border border-gray-800 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-              <option>YES!</option>
+            <select
+              onChange={e => setOption2(e.target.value)}
+              className="pointer block appearance-none w-full border border-gray-800 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+            >
+              <option>---</option>
+              <option>Yes</option>
               <option>No</option>
             </select>
             <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
@@ -69,7 +190,11 @@ const WhatToDrink = () => {
             <p className="text-xl">What is the Current Season?</p>
           </label>
           <div className="inline-block relative w-full">
-            <select className="pointer block appearance-none w-full border border-gray-800 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+            <select
+              onChange={e => setOption3(e.target.value)}
+              className="pointer block appearance-none w-full border border-gray-800 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+            >
+              <option>---</option>
               <option>Spring</option>
               <option>Summer</option>
               <option>Fall</option>
@@ -90,10 +215,10 @@ const WhatToDrink = () => {
         </div>
         <div className="flex items-center justify-between">
           <button
-            className="duration-200 bg-red-800 hover:bg-red-700 text-gray-100 py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full mx-auto"
-            type="button"
+            onClick={onSubmitForm}
+            className="duration-200 bg-red-800 hover:bg-red-700 text-gray-100 py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full mx-auto mt-4"
           >
-            Yes, Tell Me..
+            Tell Me What To Drink
           </button>
         </div>
       </form>
